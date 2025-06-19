621a355a28bc507884ba56362de4f34c5645fd5a
Teerapon pichaikan <phichaykalthiraphl@gmail.com>
2025-06-19 15:57:50 +0700

    15_Teerapon_W03

---
+++ w06.py
@@ -0,0 +1,9 @@
+# List
+numbers = []
+
+colors = ["Rad","Green","Blue","Yellow"]
+numbers = [1,10,99,45,33,11]
+
+numbers_1_20 = list(range(1,20))
+numbers_1_100 = list(range(1,101,1))
+print(numbers_1_100)
---
+++ w07.py
@@ -0,0 +1,5 @@
+colors = ["Rad","Green","Blue","Yellow"]
+
+#print(f"{colors[0]} {colors[1]} {colors[2]}")
+
+print(f"{colors[-3:]}")
